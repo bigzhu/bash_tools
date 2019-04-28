a36f174150a50b9b571a1b2108c319e872df5d12
bigzhu <vermiliondun@gmail.com>
2019-04-28 10:56:03 +0800

    add help to install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,52 @@
 #! /bin/bash
 
-if [ $1 ] && [ $1 = "-f" ] ;then
-    echo "force repalce link!";
-    sudo ln -sf ${PWD}/*.sh  /usr/local/bin/
-else
-    sudo ln -s ${PWD}/*.sh  /usr/local/bin/
-fi
+force=false
 
+help(){
+  echo "Usage: install.sh file_name [-f]"
+
+  echo ""
+  echo "Flags:"
+  echo "  -f"
+  echo "    force install if File exists(will repalce!)"
+  echo "  -h"
+  echo "    show help info"
+  echo ""
+  echo "Example:"
+  echo "  one file:"
+  echo "    install.sh ssh.py"
+  echo "  multiple file:"
+  echo "    install.sh *.py"
+  echo  " force install:"
+  echo "    install.sh *.py -f"
+}
+
+
+# check is need force install
+for param in "$@"
+do
+  if [ $param = "-f" ]; then
+    #echo "$param force is true"
+    force=true;
+  elif [ $param = "-h" ]; then
+    help;
+  fi
+done
+
+
+if [ $1 ];then
+  for param in "$@"
+  do
+    if [ $param != "-f" ] && [ $param != "-h" ]; then
+      if [ $force = true ];then
+        echo "force install $param to /usr/local/bin/";
+        sudo ln -sf ${PWD}/$param  /usr/local/bin/
+      else
+        echo "install $param to /usr/local/bin/";
+        sudo ln -s ${PWD}/$param  /usr/local/bin/
+      fi
+    fi
+  done
+else
+  help;
+fi
