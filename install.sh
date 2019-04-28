91252bebe39fcb3d82a4f9a879f615adf4c651cd
bigzhu <vermiliondun@gmail.com>
2019-04-28 09:58:43 +0800

    remove fuck

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ if [ $1 ] && [ $1 = "-f" ] ;then
     echo "force repalce link!";
     sudo ln -sf ${PWD}/*.sh  /usr/local/bin/
 else
-    echo "fuck";
     sudo ln -s ${PWD}/*.sh  /usr/local/bin/
 fi
 
