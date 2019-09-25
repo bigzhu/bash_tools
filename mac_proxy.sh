8c06db8fcd0855e892269b34c36fc7d47c4338cd
bigzhu <vermiliondun@gmail.com>
2019-09-25 14:36:47 +0800

    自建列表, 进一步完善白名单

--- mac_proxy.sh
+++ mac_proxy.sh
@@ -5,5 +5,12 @@ networksetup -setsocksfirewallproxy "Wi-fi" 127.0.0.1 1086
 echo "socks proxy status:"
 networksetup -getsocksfirewallproxy "Wi-Fi"
 # 设置不走代理的 国内 ip 列表
-curl -L http://www.ipdeny.com/ipblocks/data/countries/cn.zone |xargs networksetup -setproxybypassdomains "Wi-Fi"  127.0.0.1 localhost
-echo "set cn pass done!"
+#curl -L http://www.ipdeny.com/ipblocks/data/countries/cn.zone |xargs networksetup -setproxybypassdomains "Wi-Fi"  127.0.0.1 localhost
+#curl -L https://raw.githubusercontent.com/HMBSbige/Text_Translation/master/chndomains.txt |xargs networksetup -setproxybypassdomains "Wi-Fi"  127.0.0.1 localhost
+#curl -L https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt |xargs networksetup -setproxybypassdomains "Wi-Fi"  127.0.0.1 localhost
+# curl -L https://raw.githubusercontent.com/ym/chnroutes2/master/chnroutes.txt |xargs networksetup -setproxybypassdomains "Wi-Fi"  127.0.0.1 localhost
+
+# update: wget https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt
+cat china_ip_list.txt |xargs -n99999 networksetup -setproxybypassdomains "Wi-Fi" 
+
+echo "set cn bypass done!"
