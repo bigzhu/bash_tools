581e1cec4f8a107cf06c83f6a5c3254d34bae18c
bigzhu <vermiliondun@gmail.com>
2019-09-30 20:30:38 +0800

    get china_ip_list.txt from real path

--- mac_proxy.sh
+++ mac_proxy.sh
@@ -11,6 +11,8 @@ networksetup -getsocksfirewallproxy "Wi-Fi"
 # curl -L https://raw.githubusercontent.com/ym/chnroutes2/master/chnroutes.txt |xargs networksetup -setproxybypassdomains "Wi-Fi"  127.0.0.1 localhost
 
 # update: wget https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt
-cat china_ip_list.txt |xargs -n99999 networksetup -setproxybypassdomains "Wi-Fi" 
-
+# get shell path
+shell_path=$(cd `dirname $0`; pwd)
+echo $shell_path
+cat $shell_path/china_ip_list.txt |xargs -n99999 networksetup -setproxybypassdomains "Wi-Fi" 
 echo "set cn bypass done!"
