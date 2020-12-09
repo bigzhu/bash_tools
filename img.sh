776ca7f2a0411a2abf10457c832537ac4aa312a9
bigzhu <vermiliondun@gmail.com>
2020-12-09 16:13:04 +0800

    img compress change to use python3

--- img.sh
+++ img.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-blog_img_path="$HOME/Project/blog/cheese/assets/img"
+# brew install mozjpeg
+# brew install trash
+blog_img_path="$HOME/Dropbox/cheese/assets/img"
 url_path="/assets/img"
 # 剔除输入的文件的路径
 file_name=$(basename "$1")
@@ -19,12 +21,14 @@ fi
 
 
 # encode file name
-new_file_name=$(echo $new_file_name | python -c "import urllib.parse;print (urllib.parse.quote(input()))")
+#new_file_name=$(echo $new_file_name | python -c "import urllib.parse;print (urllib.parse.quote(input()))")
+new_file_name=$(echo $new_file_name | python3 -c "import urllib.parse;print (urllib.parse.quote(input()))")
 # markdow 格式
 #img="![$new_file_name]($url_path/$new_file_name)"
-img="{{< figure src=\"$url_path/$new_file_name\" width=\"40%\" >}}"
+#img="{{< figure src=\"$url_path/$new_file_name\" width=\"100%\" >}}"
+img="![](https://bigzhu.net$url_path/$new_file_name)"
 # 复制到剪贴板
 echo $img | pbcopy
 echo "image compress done!"
 # 原始文件移动到回收站, 小心使用
-trash "$1"
+#trash "$1"
