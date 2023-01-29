d08d72d35ff449804ecc225c2717fc6fc7088382
bigzhu <vermiliondun@gmail.com>
2023-01-29 12:20:44 +0800

    添加 c.sh

---
+++ c.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+file_type='?'
+if [ $# != 1 ]; then
+	echo "parameter error"
+else
+	len3=$(xxd -p -l 3 "$1")
+	len4=$(xxd -p -l 4 "$1")
+	3c3f78
+	if [ "$len3" == "ffd8ff" ]; then
+		file_type='jpg'
+		echo "The type is jpg"
+	elif [ "$len4" == "3c3f786d" ]; then
+		file_type='svg'
+		echo "The type is svg"
+	elif [ "$len4" == "89504e47" ]; then
+		file_type='png'
+		echo "The type is png"
+	elif [ "$len4" == "47494638" ]; then
+		file_type='gif'
+		echo "The type is gif"
+	elif [ "$len4" == "52494646" ]; then
+		file_type='webp'
+		echo "The type is webp"
+	elif [ "$len4" == "52617221" ]; then
+		file_type='rar'
+		echo "The type is rar"
+	else
+		file_type='?'
+		echo "The type is others"
+		echo "$len4"
+	fi
+fi
+if [ "$file_type" = "svg" ] || [ "$file_type" = "webp" ] || [ "$file_type" = "gif" ] || [ "$file_type" = "jpg" ] || [ "$file_type" = "png" ]; then
+	exec img.sh $1 $file_type
+else
+	exec file.sh $1
+fi
--- img.sh
+++ img.sh
@@ -1,48 +1,29 @@
 #! /bin/bash
 # brew install mozjpeg trash
 # 避免不要使用 jpeg-turbo 带的 cjpeg, 这是个垃圾
-blog_img_path="$HOME/Dropbox/cheese/images"
-url_path="./images"
+blog_img_path="$HOME/cheese/images"
+url_path="images"
 # 剔除输入的文件的路径
 file_name=$(basename "$1")
-# 分割名字和后缀
-name=$(echo "$file_name" | sed -e 's/\.[^.]*$//')
-# 后缀
-suffix=$(echo "$file_name"  | sed 's/.*\.//')
-
-if [ $suffix == "svg" ]; then
-  new_file_name=$file_name
-else
-  new_file_name="$name.jpg"
-fi
 
 # 检查文件是否存在
-if [ -f "$blog_img_path/$new_file_name" ]; then
-    echo "$blog_img_path/$new_file_name exists." 
-    echo "you need check the file name!!!"
-    exit 1
+if [ -f "$blog_img_path/$file_name" ]; then
+	echo "$blog_img_path/$file_name exists."
+	echo "you need check the file name!!!"
+	exit 1
 fi
-
-if [ $suffix == "svg" ]; then
-  mv $1 $blog_img_path/
-else
-  echo "compress $file_name"
-  #/opt/homebrew/opt/mozjpeg/bin/cjpeg -quality 44 "$1" > "$blog_img_path/$new_file_name"
-  /opt/homebrew/opt/mozjpeg/bin/cjpeg "$1" > "$blog_img_path/$new_file_name"
-fi
-
-
 # encode file name
-#new_file_name=$(echo $new_file_name | python -c "import urllib.parse;print (urllib.parse.quote(input()))")
-new_file_name=$(echo $new_file_name | python3 -c "import urllib.parse;print (urllib.parse.quote(input()))")
-# markdow 格式
-#img="![$new_file_name]($url_path/$new_file_name)"
-#img="{{< figure src=\"$url_path/$new_file_name\" width=\"100%\" >}}"
-img="![$name]($url_path/$new_file_name)"
-# 复制到剪贴板
-echo $img | pbcopy
-echo "image compress done!"
-# 原始文件移动到回收站, 小心使用
-if [ $suffix != "svg" ]; then
-  trash "$1"
+encode_file_name=$(echo $file_name | python3 -c "import urllib.parse;print (urllib.parse.quote(input()))")
+markdow_img_link="![$file_name]($url_path/$encode_file_name)"
+# 无需压缩的图片类型
+if [ "$2" = "svg" ] || [ "$2" = "webp" ] || [ "$2" = "gif" ]; then
+	mv $1 $blog_img_path/
+else
+	#/opt/homebrew/opt/mozjpeg/bin/cjpeg -quality 44 "$1" > "$blog_img_path/$new_file_name"
+	echo "compress $file_name"
+	/opt/homebrew/opt/mozjpeg/bin/cjpeg "$1" >"$blog_img_path/$file_name"
+	trash "$1"
 fi
+
+echo $markdow_img_link | pbcopy
+echo "image compress done!"
