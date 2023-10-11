0f97ca939c05c2905b4ee2403a5e5a50dff7c05b
bigzhu <vermiliondun@gmail.com>
2023-10-11 19:45:53 +0800

    自动打卡 fix

--- ding.sh
+++ ding.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 # 钉钉的自动打卡
 #
-function rand(){
-    min=$1
-    max=$(($2-$min+1))
-    num=$(date +%s%N)
-    echo $(($num%$max+$min))
+export PATH=$PATH:/home/bigzhu/Android/Sdk/platform-tools/
+function rand() {
+	min=$1
+	max=$(($2 - $min + 1))
+	num=$(date +%s%N)
+	echo $(($num % $max + $min))
 }
 
-rnd=$(rand 10 20)
+# 唤醒
+adb -s FA69T0307042 shell input keyevent 26
+adb -s FA69T0307042 shell input swipe 300 1700 300 500
+adb -s FA69T0307042 shell input text z129854 && adb -s FA69T0307042 shell input keyevent 66
 
-adb shell am start -n com.alibaba.android.rimet/com.alibaba.android.rimet.biz.LaunchHomeActivity
+adb -s FA69T0307042 shell am start -n com.alibaba.android.rimet/com.alibaba.android.rimet.biz.LaunchHomeActivity
 rnd=$(rand 10 20)
 sleep "${rnd}s"
-adb shell input tap 579 1722
+adb -s FA69T0307042 shell input tap 579 1722
 rnd=$(rand 10 20)
 sleep "${rnd}s"
-adb shell input tap 330 1014
+adb -s FA69T0307042 shell input tap 330 1014
 rnd=$(rand 10 20)
 sleep "${rnd}s"
-adb shell input tap 507 1133
-rnd=$(rand 10 20)
-sleep "${rnd}s"
-adb shell  am force-stop com.alibaba.android.rimet
-rnd=$(rand 10 20)
-sleep "${rnd}s"
-adb shell input keyevent 26
+adb -s FA69T0307042 shell input tap 507 1133
+sleep "2s"
+adb -s FA69T0307042 shell am force-stop com.alibaba.android.rimet
+sleep "6s"
+adb -s FA69T0307042 shell input keyevent 26
