00c4014500cf553c82411a64660def800b410134
bigzhu <vermiliondun@gmail.com>
2023-11-05 14:00:44 +0800

    force link

--- link.sh
+++ link.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
 # config
-ln -s ~/Sync/config/nvim ~/.config
-ln -s ~/Sync/config/kitty ~/.config
+ln -sf ~/Sync/config/nvim ~/.config
+ln -sf ~/Sync/config/kitty ~/.config
 
 # home
-ln -s ~/Sync/home/ssh ~/.ssh
-ln -s ~/Sync/home/zshrc ~/.zshrc
-ln -s ~/Sync/home/oh-my-zsh ~/.oh-my-zsh
-ln -s ~/Sync/home/p10k.zsh ~/.p10k.zsh
-ln -s ~/Sync/home/gitconfig ~/.gitconfig
+ln -sf ~/Sync/home/ssh ~/.ssh
+ln -sf ~/Sync/home/zshrc ~/.zshrc
+ln -sf ~/Sync/home/oh-my-zsh ~/.oh-my-zsh
+ln -sf ~/Sync/home/p10k.zsh ~/.p10k.zsh
+ln -sf ~/Sync/home/gitconfig ~/.gitconfig
+
+# Library
+ln -sf ~/Sync/Library/Rime ~/Library
